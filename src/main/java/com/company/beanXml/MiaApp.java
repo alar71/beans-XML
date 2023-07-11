a53059659d5ea5889f42fc92ed4a16f45e7a7d74
alar71 <71129664+alar71@users.noreply.github.com>
2023-07-11 20:43:54 +0200

    .

--- src/main/java/com/company/beanXml/MiaApp.java
+++ src/main/java/com/company/beanXml/MiaApp.java
@@ -9,7 +9,7 @@ public class MiaApp {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         // Llamamos al ID creado
-        Factura factura = context.getBean("miFactura", Factura.class);
+        Documentos factura = context.getBean("miFactura", Factura.class);
 
         // Mostramos en este caso el metodo de ejemplo creado
         System.out.println(factura.TipoDocumento());
